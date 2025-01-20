14e4ed5d76331e98a0d9a4116bc2320321ac7e5b
Preyapradhan <Preyapradhan2645@gmail.com>
2025-01-20 12:58:23 +0530

    Remove config.py from repository

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-#CRUNCHBASE_URL = "https://api.crunchbase.com/api/v4/entities/organizations/crunchbase?user_key={ca8b00c5db7f0ec5be7ea41448bb22ec}"
-CRUNCHBASE_URL="https://www.crunchbase.com/"
-CHROME_DRIVER_PATH = "C:\\Users\\Preya\\Downloads\\chromedriver-win32"
-GOOGLE_API_KEY = "AIzaSyC5cOfhJX2sJJ9kCPtK2VtQPtSIgfVS17o"
-GOOGLE_CX = "b103e058667fc4cf0"
-OPENAI_API_KEY = "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"
